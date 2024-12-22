864c82f453f5b42330cd6ec438074852691edf9f
arzaqqq <mmiftahularzaq01@gmail.com>
2024-12-22 23:12:40 +0700

    progress sudah bsia tamabh tombol X pada game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Square({ value, onSquareClick }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    // Salin array menggunakan slice()
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X'; // Ubah nilai pada indeks tertentu
+    setSquares(nextSquares); // Perbarui state
+  }
+
+  return (
+    <div className="board">
+      {squares.map((value, index) => (
+        <Square
+          key={index}
+          value={value}
+          onSquareClick={() => handleClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
